b4322223d098536c726e40a58b121b5618f4ea01
phj1027 <qkrgywjd981027@naver.com>
2021-11-03 23:10:04 +0900

    Init Mushroom

--- character_move.py
+++ character_move.py
@@ -5,6 +5,7 @@ open_canvas(800, 450)
 background = load_image('background.png')
 character_wr = load_image('walk_animation_mario.png')
 character_wl = load_image('walk_animation_mario_left.png')
+mushroom = load_image('mushroom_animation.png')
 
 def handle_events():
     global running
@@ -31,13 +32,15 @@ def handle_events():
 running = True
 x = 800 // 2 #캐릭터 가운데위치
 frame = 0 #3개
+frame_m =0
 dir = 0 #방향변수 -1left +1right
-state = 0 #키를 뗐을때 방향상태 2right 3left
 
 while running:
     clear_canvas()
     background.draw(400, 225)
     character_wr.clip_draw(frame * 55, 0, 55, 30, x, 75)
+    mushroom.clip_draw(frame_m * 25, 0, 25, 25, 500, 74)
+    frame_m = (frame_m + 1) % 4
 
     if dir == 1:
         character_wr.clip_draw(frame * 55, 0, 55, 30, x, 75)
